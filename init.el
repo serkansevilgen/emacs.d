bad4198f3b041fe2bcf9fff3fa3d160740a3d690
Serkan Sevilgen <ssevilgen@gmail.com>
2015-02-19 21:54:25 +0200

    js-comint added for node repl

--- init.el
+++ init.el
@@ -112,3 +112,15 @@
 (custom-set-faces 
  '(flymake-errline ((t (:background "DarkRed")))) 
  '(flymake-warnline ((((class color)) (:background "DarkBlue"))))) 
+
+;; js-comint
+(require 'js-comint)
+(setq inferior-js-program-command "node --interactive")
+(setenv "NODE_NO_READLINE" "1")
+(add-hook 'js2-mode-hook '(lambda () 
+			    (local-set-key "\C-x\C-e" 'js-send-last-sexp)
+			    (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
+			    (local-set-key "\C-cb" 'js-send-buffer)
+			    (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)
+			    (local-set-key "\C-cl" 'js-load-file-and-go)
+			    ))
